c009f01517e08ed811a8b84ffc0b4f23ac0d0a92
jing <shajingyuan@me.com>
2025-07-17 11:41:22 +0200

    update deepseek

--- scripts/baseline_models/deepseek.py
+++ scripts/baseline_models/deepseek.py
@@ -86,7 +86,7 @@ def infer_logic_rules(model, processor, train_positive, train_negative, device, 
 
     inputs_embeds = model.prepare_inputs_embeds(**prepare_inputs)
 
-    outputs = model.language.generate(
+    outputs = model.generate(
         inputs_embeds=inputs_embeds,
         attention_mask=prepare_inputs.attention_mask,
         pad_token_id=processor.tokenizer.eos_token_id,
