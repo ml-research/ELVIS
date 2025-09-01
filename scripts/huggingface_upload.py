468f6bc0eb082ecc969982745eab7aed30172b69
jing <shajingyuan@me.com>
2025-09-01 14:28:31 +0200

    fix a bug

--- scripts/huggingface_upload.py
+++ scripts/huggingface_upload.py
@@ -3,35 +3,46 @@ from huggingface_hub import HfApi, HfFolder, Repository
 import os
 import shutil
 from pathlib import Path
+import argparse
 
 
 from scripts import config
 
-# ==== CONFIG ====
-username = "akweury"  # change this!
-repo_name = "ELVIS"
-source_folder = Path("/Users/jing/PycharmProjects/ELVIS/data/raw_patterns/res_1024_pin_False")
-upload_folder = Path("./upload_repo")  # local working dir
-# ===============
+def upload_to_huggingface(args):
+    # ==== CONFIG ====
+    username = "akweury"
+    repo_name = "ELVIS"
+    if args.remote:
+        source_folder = f"/home/ml-jsha/storage/ELVIS_Data/res_{args.resolution}_pin_False"
+    else:
+        source_folder = Path(f"/Users/jing/PycharmProjects/ELVIS/data/raw_patterns/res_{args.resolution}_pin_False")
 
-# Full repo ID
-repo_id = f"{username}/{repo_name}"
+    upload_folder = Path("./upload_repo")  # local working dir
+    # ===============
+    # Full repo ID
+    repo_id = f"{username}/{repo_name}"
+    # Cleanup old repo if exists
+    if upload_folder.exists():
+        shutil.rmtree(upload_folder)
+    # Create the HF repo if not already
+    api = HfApi()
+    api.create_repo(repo_id, repo_type="dataset", exist_ok=True)
+    # Clone the repo locally
+    repo = Repository(local_dir=str(upload_folder), clone_from=repo_id, repo_type="dataset")
+    # Copy source folder into cloned repo
+    shutil.copytree(source_folder, upload_folder / "closure")
+    # Commit and push
+    repo.push_to_hub(commit_message=f"Upload (res_{args.resolution}, pin_False)")
+    print(f"✅ Uploaded successfully to https://huggingface.co/datasets/{repo_id}")
 
-# Cleanup old repo if exists
-if upload_folder.exists():
-    shutil.rmtree(upload_folder)
 
-# Create the HF repo if not already
-api = HfApi()
-api.create_repo(repo_id, repo_type="dataset", exist_ok=True)
+def main():
+    parser = argparse.ArgumentParser(description="Evaluate baseline models with CUDA support.")
+    parser.add_argument("--remote", action="store_true")
+    parser.add_argument("--resolution", type=int, default=224, choices=[224, 448, 1024])
+    args = parser.parse_args()
+    upload_to_huggingface(args)
 
-# Clone the repo locally
-repo = Repository(local_dir=str(upload_folder), clone_from=repo_id, repo_type="dataset")
 
-# Copy source folder into cloned repo
-shutil.copytree(source_folder, upload_folder / "closure")
-
-# Commit and push
-repo.push_to_hub(commit_message="Upload closure (res_1024, pin_False)")
-
-print(f"✅ Uploaded successfully to https://huggingface.co/datasets/{repo_id}")
+if __name__ == "__main__":
+    main()
